89011b36c5e843cd60d1fbda773560e7af18f454
Samar Ranjit <samarranjit321@gmail.com>
2026-02-12 15:05:18 -0600

    fix: adjust padding for activity items in OtherMember component

--- src/app/people/OtherMember.tsx
+++ src/app/people/OtherMember.tsx
@@ -191,7 +191,7 @@ function OtherMember() {
                                   <div
                                     key={`${selectedMember.id}-activity-${index}`}
                                     className={`relative group flex items-start gap-4 p-4 ${
-                                      activity?.label ? "md:pt-[3rem]" :
+                                      activity?.label ? "pt-[3rem] md:pt-[3.5rem]" :
                                         ""
                                     } sm:p-6 bg-slate-50 rounded-xl hover:bg-slate-100 transition-all duration-200 hover:shadow-sm border border-slate-100/50`}
                                   >
